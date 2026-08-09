ca3a3e2e5f3b926f51a3d4205abec8f0bfdf552a
Amornthep225 <128355119+Amornthep225@users.noreply.github.com>
2026-08-09 18:45:59 +0700

    แก้ err

--- app/staff/staff-sos/page.jsx
+++ app/staff/staff-sos/page.jsx
@@ -1,19 +1,8 @@
 "use client";
 
-<<<<<<< Updated upstream
-import StaffLayout from "@/components/layout/StaffLayout";
-=======
 
-import { colors } from "@/constants/colors";
-<<<<<<< Updated upstream
-import { cards } from "@/constants/cards";
-import { buttons } from "@/constants/buttons";
-import StaffNavbar from "@/components/staff/staff-navbar";
-=======
 import StaffLayout from "@/components/layout/StaffLayout";
->>>>>>> Stashed changes
-const theme = colors.staff;
->>>>>>> Stashed changes
+
 
 import StaffSos from "@/components/staff/StaffSosPage/StaffSos";
 
